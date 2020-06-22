f1cefe5658fe126579b79d9fa54c3986f54fff3b
geoffrey.adam <geoffrey.adam@ymail.com>
2020-06-22 21:15:06 +0200

    heroku

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Article;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('articles/{token}', 'ArticleController@index');
+Route::get('postArticle/{token}', 'ArticleController@store');
+Route::get('articles/{article}', 'ArticleController@show');
+// Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
